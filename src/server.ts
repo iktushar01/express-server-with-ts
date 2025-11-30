7f2210f7bf079bc2351cdc8b6e85298d0244ef53
tushar <iktushar2020@gmail.com>
2025-11-30 09:35:02 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,79 @@
+import express, {Request, Response} from 'express';
+import {Pool} from "pg";
+import dotenv from "dotenv";
+import path from "path";
+
+// Load environment variables from .env file
+dotenv.config({path: path.join(process.cwd(), '.env')});
+
+
+// Create an Express application
+const app = express()
+const port = 5000;
+
+
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+
+// PostgreSQL connection pool
+const pool = new Pool({
+  connectionString: `${process.env.CONNECTION_STRING}`,
+});
+
+const initDB = async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    age INT NOT NULL,
+    phone VARCHAR(15),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    );
+  `)
+
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS todos (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    is_completed BOOLEAN DEFAULT FALSE,
+    due_date DATE,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    );
+    `)
+
+}
+
+// Initialize the database
+initDB().then(() => {
+  console.log("Database initialized");
+}).catch((err) => {
+  console.error("Error initializing database:", err);
+});
+
+// Test route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello!')
+})
+
+// POST route to handle incoming JSON data
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(200).json({ 
+    success: true,
+    message: "POST request received" });
+} );
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
